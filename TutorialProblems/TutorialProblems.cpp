2a90c123c3428afb4d69e351d7c95a3d84a00096
kksmash123 <kksmashes@gmail.com>
2022-08-15 16:50:16 +0530

    largest element

--- TutorialProblems/TutorialProblems.cpp
+++ TutorialProblems/TutorialProblems.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+
 
 using namespace std;
+
+
+int LargestElement(vector<int> v) 
+{
+	int max_ele1=INT_MIN;
+	int max_ele2 = INT_MIN;
+
+	for (int x : v)
+		cout << x << " ";
+	cout << endl;
+
+	//basic method
+	for (int i = 0;i < v.size();i++)
+		max_ele1 = v[i] > max_ele1 ?v[i] : max_ele1;
+	cout << "Max element (method 1) -> " << max_ele1 << endl;
+
+	//in built function to find max element 
+	max_ele2 = *max_element(v.begin(), v.end());
+	cout <<"Max element (method 2) -> " << max_ele1<<endl;
+
+	// for normal array 
+	int a[] = { -2,3,4,-1,5,-12,6,1,-13 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+	cout <<"using max_element -> " << *max_element(a, a + n) << endl;
+	cout <<"using min_element -> " << *min_element(a, a + (n - 1)) << endl;
+
+	//single line code without using the variable
+	return *max_element(v.begin(), v.end());
+}
+
+
+
+
+
 
 
 //using kadane's algorithm in maximum subarray sum -> o(n) time complexity
@@ -94,5 +132,8 @@ int main()
 	int n = sizeof(a) / sizeof(a[0]);
 
 	cout<<subarraySum3(a, n);*/
+
+	vector<int> v = { -2,3,4,-1,5,-12,6,1,3 };
+	cout<<"output -> " << LargestElement(v);
 
 }
